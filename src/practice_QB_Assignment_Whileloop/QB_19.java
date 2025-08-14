089f0a69140992a5fd8bac98089e2bab48959ce1
asus <asus@DESKTOP-ASIKSSU>
2025-08-14 12:05:29 +0530

    Update

---
+++ src/practice_QB_Assignment_Whileloop/QB_19.java
@@ -0,0 +1,15 @@
+package practice_QB_Assignment_Whileloop;
+
+public class QB_19 {
+public static void main(String[] args) {
+	int a=432;
+	int product=1;
+	
+	while(a>0){
+		int b = a%10;  // get last digit
+		product = product*b;  // logic of product is     right to left value move 2*3=6 ; 6*4=24
+		a/=10;					// remove last digit
+	}
+	System.out.println("The Product value is:"+product);
+}
+}
